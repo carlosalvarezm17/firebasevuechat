86c50141ec17169038b8df92840dc1c19831e5c5
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-07-02 14:29:46 -0500

    Init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase'
+
+// Required for side-effects
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDLqmvlFVXPFYsosNIXHfy3YJYFIZYGFsU",
+  authDomain: "fir-vuechat-a8a04.firebaseapp.com",
+  databaseURL: "https://fir-vuechat-a8a04.firebaseio.com",
+  projectId: "fir-vuechat-a8a04",
+  storageBucket: "fir-vuechat-a8a04.appspot.com",
+  messagingSenderId: "442539619635",
+  appId: "1:442539619635:web:43b3e4656a6de6396cba46"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+window.db = db;
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>Login</h3>
+        <button @click="login">Login with google</button>        
+    </div>
+</template>
+
+<script>
+
+import firebase from 'firebase'
+export default {
+    methods:{
+        login(){
+            var provider = new firebase.auth.GoogleAuthProvider();
+        }
+    }
+}
+</script>
