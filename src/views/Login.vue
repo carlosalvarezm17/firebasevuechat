123429e51f7f706a55547f8e9345ac28873cd519
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-07-02 15:01:05 -0500

    Cambios this

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,35 @@
 <template>
-    <div>
-        <h3>Login</h3>
-        <button @click="login">Login with google</button>        
-    </div>
+  <div>
+    <h3>Login</h3>
+    <button @click="login">Login with google</button>
+  </div>
 </template>
 
 <script>
+import firebase from "firebase";
 
-import firebase from 'firebase'
 export default {
-    methods:{
-        login(){
-            var provider = new firebase.auth.GoogleAuthProvider();
-        }
+  methods: {
+    login() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(function(result) {
+          var token = result.credential.accessToken;
+          var user = result.user;
+            console.log(user);
+          this.$router.push('/');
+        })
+        .catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          console.log(errorMessage);
+        });
     }
-}
+  }
+};
 </script>
